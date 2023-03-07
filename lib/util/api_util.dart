a92b0187b5ec7a293171427d81c679fac70b94b7
Hsinwei Chen <7691822+edwinyoo44@users.noreply.github.com>
2023-03-07 15:31:21 +0800

    remove no use api

--- lib/util/api_util.dart
+++ lib/util/api_util.dart
@@ -35,37 +35,6 @@ class APIUtil {
     return json;
   }
 
-  static Future<String> getSentencesByID(sentencesID) async {
-
-    final response = await http.post(
-      Uri.https('api.alicsnet.com', 'app/sentence/getSentencesByID'),
-      headers: <String, String>{
-        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-      },
-      body: {
-        'sentencesID': sentencesID.toString(),
-      },
-    );
-    String json = response.body.toString();
-    return json;
-  }
-
-  static Future<String> getPhoneticExercisesSentencesByWordSet(String learningClassification, String learningPhase) async {
-
-    final response = await http.post(
-      Uri.https('api.alicsnet.com', 'app/sentence/getPhoneticExercisesSentencesByWordSet'),
-      headers: <String, String>{
-        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-      },
-      body: {
-        'learningClassification': learningClassification,
-        'learningPhase': learningPhase,
-      },
-    );
-    String json = response.body.toString();
-    return json;
-  }
-
   static Future<String> checkSentences(String questionText, String answerText, {int correctCombo:0}) async {
     final response = await http.post(
       Uri.https('api.alicsnet.com', 'app/sentence/checkSentences'),
@@ -96,73 +65,7 @@ class APIUtil {
     String json = response.body.toString();
     return json;
   }
-
-
-  static Future<String> sendMessageToConversation(String accessToken, String conversationID, String message) async {
-
-    final response = await http.post(
-      Uri.https('api.alicsnet.com', 'app/rasa/sendMessageToConversation'),
-      headers: <String, String>{
-        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-      },
-      body: {
-        'accessToken': accessToken,
-        'conversationID': conversationID,
-        'message': message,
-      },
-    );
-    String json = response.body.toString();
-    return json;
-  }
-  static Future<void> getConversationTokenAndID() async {
-    final response = await http.get(
-      Uri.https('api.alicsnet.com', 'app/rasa/getConversationTokenAndID'),
-    );
-    String json = response.body.toString();
-    var data = jsonDecode(json.toString());
-    if(data['apiStatus'] == 'success'){
-      //print(data['data']['accessToken']);
-      SharedPreferencesUtil.saveData<String>('applicationSettingsDataAccessToken', data['data']['accessToken']);
-      SharedPreferencesUtil.saveData<String>('applicationSettingsDataConversationID', data['data']['conversationID']);
-    } else {
-      //print('_responseAPI Error apiStatus:' + data['apiStatus'] + ' apiMessage:' + data['apiMessage']);
-      sleep(Duration(seconds:1));
-      getConversationTokenAndID();
-    }
-
-  }
-
-  static Future<String> getQuizHistory(String uuid) async {
-
-    final response = await http.post(
-      Uri.https('api.alicsnet.com', 'app/quiz/getQuizHistory'),
-      headers: <String, String>{
-        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-      },
-      body: {
-        'uuid': uuid,
-      },
-    );
-    String json = response.body.toString();
-    return json;
-  }
-
-  static Future<String> getQuizDataByID(quizID, String uuid) async {
-
-    final response = await http.post(
-      Uri.https('api.alicsnet.com', 'app/quiz/getQuizDataByID'),
-      headers: <String, String>{
-        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-      },
-      body: {
-        'quizID': quizID.toString(),
-        'uuid': uuid,
-      },
-    );
-    String json = response.body.toString();
-    return json;
-  }
-
+  
   static Future<String> saveQuizData(String uuid, String quizTitle, List<int> sentenceIDArray, List<String> sentenceAnswerArray, List<int> scoreArray, List<int> secondsArray) async {
 
     final response = await http.post(
@@ -203,21 +106,6 @@ class APIUtil {
     return json;
   }
 
-  static Future<String> minimalPairOneFinder(ipa) async {
-
-    final response = await http.post(
-      Uri.https('api.alicsnet.com', 'app/minimalPair/oneFinder'),
-      headers: <String, String>{
-        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-      },
-      body: {
-        'ipa': ipa.toString(),
-      },
-    );
-    String json = response.body.toString();
-    return json;
-  }
-
   static Future<String> minimalPairTwoFinder(ipa1, ipa2, {String dataLimit = ''}) async {
 
     final response = await http.post(
@@ -250,73 +138,7 @@ class APIUtil {
     String json = response.body.toString();
     return json;
   }
-
-  static Future<String> checkPronunciation(String questionText, String answerText) async {
-
-    final response = await http.post(
-      Uri.https('api.alicsnet.com', 'app/sentence/checkSentences'),
-      headers: <String, String>{
-        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-      },
-      body: {
-        'questionText': questionText,
-        'answerText': answerText,
-      },
-    );
-    String json = response.body.toString();
-    return json;
-  }
-
-  static Future<String> getWordLearning(String learningClassification, String learningPhase) async {
-
-    final response = await http.post(
-      Uri.https('api.alicsnet.com', 'app/word/getWordLearning'),
-      headers: <String, String>{
-        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-      },
-      body: {
-        'learningClassification': learningClassification,
-        'learningPhase': learningPhase,
-      },
-    );
-    String json = response.body.toString();
-    return json;
-  }
-
-
-  static Future<String> getWordSetList(String uid, String learningClassification) async {
-
-    final response = await http.post(
-      Uri.https('api.alicsnet.com', 'app/word/getWordSetList'),
-      headers: <String, String>{
-        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-      },
-      body: {
-        'uid': uid,
-        'learningClassification': learningClassification,
-      },
-    );
-    String json = response.body.toString();
-    return json;
-  }
-
-  static Future<String> addWordSet(String uid, String learningClassification) async {
-
-    final response = await http.post(
-      Uri.https('api.alicsnet.com', 'app/word/addWordSet'),
-      headers: <String, String>{
-        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-      },
-      body: {
-        'uid': uid,
-        'learningClassification': learningClassification,
-      },
-    );
-    String json = response.body.toString();
-    return json;
-  }
-
-
+  
   static Future<String> getSentenceTopicData() async {
 
     final response = await http.get(
@@ -325,77 +147,7 @@ class APIUtil {
     String json = response.body.toString();
     return json;
   }
-
-  static Future<String> getWordSetClassificationData() async {
-
-    final response = await http.get(
-      Uri.https('api.alicsnet.com', 'app/word/getWordSetClassificationData'),
-    );
-    String json = response.body.toString();
-    return json;
-  }
-
-  static Future<String> getWordSetTotalList(String index) async {
-
-    final response = await http.post(
-      Uri.https('api.alicsnet.com', 'app/word/getWordSetTotalList'),
-      headers: <String, String>{
-        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-      },
-      body: {
-        'index': index,
-      },
-    );
-    String json = response.body.toString();
-    return json;
-  }
-
-  static Future<String> getWordList(String index, {String dataLimit = ''}) async {
-
-    final response = await http.post(
-      Uri.https('api.alicsnet.com', 'app/word/getWordList'),
-      headers: <String, String>{
-        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-      },
-      body: {
-        'index': index,
-        'dataLimit': dataLimit,
-      },
-    );
-    String json = response.body.toString();
-    return json;
-  }
-
-  static Future<String> getWordRowIndex(String word) async {
-
-    final response = await http.post(
-      Uri.https('api.alicsnet.com', 'app/word/getWordRowIndex'),
-      headers: <String, String>{
-        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-      },
-      body: {
-        'word': word,
-      },
-    );
-    String json = response.body.toString();
-    return json;
-  }
-
-  static Future<String> getWordData(String word) async {
-
-    final response = await http.post(
-      Uri.https('api.alicsnet.com', 'app/word/getWordData'),
-      headers: <String, String>{
-        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-      },
-      body: {
-        'word': word,
-      },
-    );
-    String json = response.body.toString();
-    return json;
-  }
-
+  
   static Future<String> getIPAAvailable() async {
 
     final response = await http.get(
