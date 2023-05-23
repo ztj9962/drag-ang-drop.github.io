3bb6b525990fe06596a2b115a3d7f666e27dde7c
TMQPR <75326607+TMQPR@users.noreply.github.com>
2023-05-24 03:49:14 +0800

    add practice button, change introduce content, change page toggle button

--- lib/page/index/index_pronunciation_page.dart
+++ lib/page/index/index_pronunciation_page.dart
@@ -20,26 +20,32 @@ class _IndexPronunciationPageState extends State<IndexPronunciationPage> {
   bool? _isSignin = false;
   int _testIndex = 0;
 
-  List<String> _title = ['Pronunciation', 'IPA Grapheme', 'Contraction', 'Minimal Pair', 'Harvard', 'Tongue Twisters'];
-  List<String> _svgName = ['practicePronunciation', 'practiceIPA', 'practiceContraction', 'practiceMinimalPair', 'practiceHarvard', 'practiceTongueTwisters'];
+  List<String> _title = ['發音練習有什麼?', 'IPA字素練習', '縮寫練習', '相似字音練習', 'Harvard句子練習', '英語繞口令練習', '總結'];
+  List<String> _routeName = ['ipa_grapheme_pair_index_page', 'contraction_index_page', 'minimal_pair_index', 'harvard_index_page', 'tongue_twisters_index_page'];
+  List<String> _svgName = ['practicePronunciation', 'practiceIPA', 'practiceContraction', 'practiceMinimalPair', 'practiceHarvard', 'practiceTongueTwisters', 'practicePronunciation'];
   List<String> _introduction = [
-    '英語源自拉丁字母表，有 26 個字母。 \n\n'
-        '然而，由於從德語、法語和希臘語等其他語言中藉詞，英語發展出了 44 個發音。 \n\n'
-        '這造成了拼寫和發音之間的不匹配，其中字母可以代表多個聲音，聲音可以由多個字母代表。 \n\n'
-        '這種複雜性使英語學習者很難掌握這門語言。',
-    '首先，您可以識別和練習 IPA 聲音字母對照。\n\n'
-        '國際音標 (IPA) 使用符號來表示人類語言的各種聲音。 \n\n'
-        '通過將每個國際音標音映射到對應的字母或字母組，您可以了解英語中音和字母的關係，提高單詞的準確發音能力。',
-    '其次，你可以練習說縮略語。收縮涉及將多個詞合併為一個詞。 \n\n'
-        '練習縮略語可以幫助您學會更流暢地將單詞連接在一起，並實現更流暢的演講。 \n\n'
-        '使用縮略語是英語會話的一個共同特徵。 \n\n'
-        '通過學習正確使用縮略語，您可以在說英語時聽起來更自然。',
-    '第三，你可以練習最小對數。最小對是只有一個發音不同的詞對。 \n\n'
-        '通過練習最小對數，您可以幫助您識別並產生英語發音中的細微差異。',
-    '第四，你可以練習說哈佛句子。哈佛句子是 720 個示例短語的集合，分為 10 個列表，用於 IP 語音、蜂窩和其他電話系統的標準化測試。 \n\n'
-        '哈佛句子旨在快速清晰地大聲朗讀。 這可以幫助您提高發音速度和清晰度。',
-    '最後，你可以練習說繞口令。繞口令是短而重複的短語，旨在難以快速正確地說出來。 \n\n'
-        '練習繞口令可以幫助訓練您的嘴巴和舌頭重複發出相同的聲音，這可以增強您對具有挑戰性的聲音和單詞的發音。'];
+    '英語是源自拉丁字母的語言，擁有26個字母。\n\n'
+        '然而，由於從其他語言如德語、法語和希臘語借用詞彙，英語發展出了44個音素。\n\n'
+        '這造成了拼寫和發音之間的不匹配，字母可以代表多個音素，而音素也可以用多個字母表示。\n\n'
+        '這種複雜性使得英語學習者在掌握這門語言時面臨挑戰。\n\n'
+        '為了幫助您提高英語發音，我們的發音課程提供了幾種練習方法。',
+    '首先，您可以識別並練習國際音標（IPA）的音素-字母對照。\n\n'
+        '國際音標（IPA）使用符號表示人類語言的各種音素。\n\n'
+        '通過將每個國際音標音素對照到相應的字母或字母組合，您可以理解英語中音素和字母之間的關係，並提高準確發音的能力。',
+    '其次，您可以練習說縮略詞。縮略詞涉及將多個單詞合併為一個詞。\n\n'
+        '練習縮略詞有助於您學會更流暢地將單詞連接在一起，達到更流利的口語。\n\n'
+        '使用縮略詞是英語會話的一個常見特點。\n\n'
+        '通過學會正確使用縮略詞，您在講英語時可以聽起來更自然。',
+    '第三，您可以練習最小對立詞。最小對立詞是僅在一個音素上有所不同的詞對。\n\n'
+        '通過練習最小對立詞，您可以幫助自己識別和產生英語發音中的細微差異。',
+    '第四，您可以練習哈佛句子。哈佛句子是一組720個示例短語，分成10個列表，用於語音網絡電話、蜂窩和其他電話系統的標準測試。\n\n'
+        '哈佛句子設計成可以快速清晰地朗讀。這可以幫助您提高發音的速度和清晰度。',
+    '最後，您可以練習繞口令。繞口令是一種短小而重複的詞語，旨在迅速而正確地說出來會比較困難。\n\n'
+        '練習繞口令有助於訓練口腔和舌頭重複發出相同的音素，這可以提高您發音困難的音素和詞語的準確性。',
+    '這些練習方法每一個都可以通過專注於語言的特定方面來幫助您提高英語發音。\n\n'
+        '通過將這些方法融入您的學習計劃中，您可以在說英語時變得更加自信和熟練。\n\n'
+        '希望這些方法能幫助您提高英語發音。',
+  ];
 
   @override
   void initState() {
@@ -70,20 +76,28 @@ class _IndexPronunciationPageState extends State<IndexPronunciationPage> {
         actions: <Widget>[
           Padding(
             padding: EdgeInsets.only(right: 10),
-            child: ElevatedButton(
-              onPressed: (){
-                setState(() {
-                  _testIndex = 0;
-                });
-                introducePronunciationPractice();
-              },
-              child: Icon(Icons.question_mark_outlined),
-              style: ElevatedButton.styleFrom(
-                minimumSize: Size(10, 10),
-                shape: CircleBorder(),
-                padding: EdgeInsets.all(10),
-                backgroundColor: PageTheme.white,
-                foregroundColor: PageTheme.app_theme_black,
+            child: Container(
+              padding: EdgeInsets.all(4),
+              margin: EdgeInsets.all(6),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.white,
+              ),
+              child: ElevatedButton(
+                child: Icon(Icons.question_mark_outlined),
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(10, 10),
+                  shape: CircleBorder(),
+                  //padding: EdgeInsets.all(10),
+                  backgroundColor: PageTheme.app_theme_black,
+                  foregroundColor: PageTheme.white,
+                ),
+                onPressed: (){
+                  setState(() {
+                    _testIndex = 0;
+                  });
+                  introducePronunciationPractice();
+                },
               ),
             ),
           ),
@@ -189,16 +203,17 @@ class _IndexPronunciationPageState extends State<IndexPronunciationPage> {
             ),
             child: Container(
               padding: EdgeInsets.all(30),
-              width: MediaQuery.of(context).size.width*0.8,
+              width: MediaQuery.of(context).size.width*0.9,
               height: MediaQuery.of(context).size.height*0.8,
               child: Column(
-                //crossAxisAlignment: CrossAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   Expanded(
                     flex: 1,
                       child: Column(
                         children: <Widget>[
-                          Text('發音練習有什麼?', style: TextStyle(fontSize: 20),),
+                          Text(_title[_testIndex], style: TextStyle(fontSize: 20, color: PageTheme.app_theme_blue),),
                           Divider(
                             thickness: 1,
                             color: PageTheme.app_theme_blue,
@@ -207,69 +222,115 @@ class _IndexPronunciationPageState extends State<IndexPronunciationPage> {
                       )
                   ),
                   Expanded(
-                    flex: 1,
-                    child: Column(
-
-                    ),
-                  ),
-                  Expanded(
-                    flex: 5,
+                    flex: 8,
                     child: SingleChildScrollView(
                       child: Column(
                         children: <Widget>[
                           SvgPicture.asset(
                             'assets/icon/${_svgName[_testIndex]}.svg',
-                            height: 100,),
-                          Text(_introduction[_testIndex]),
-                          //Padding(padding: EdgeInsets.all(10)),
+                            height: 150,
+                          ),
+                          Padding(padding: EdgeInsets.all(6)),
+                          Text(
+                            _introduction[_testIndex],
+                            style: TextStyle(fontSize: 14),
+                          ),
+                          Row(
+                            children: <Widget>[
+                              Expanded(
+                                flex: 1,
+                                child: Container(),
+                              ),
+                              Expanded(
+                                flex: 4,
+                                child: Visibility(
+                                  visible: (_testIndex != 0) & (_testIndex < _introduction.length-1),
+                                  child: Container(
+                                    padding: EdgeInsets.all(6),
+                                    margin: EdgeInsets.all(20),
+                                    decoration: BoxDecoration(
+                                      borderRadius: const BorderRadius.all(Radius.circular(16.0)),
+                                      boxShadow: [BoxShadow(color: PageTheme.cutom_article_practice_background)],
+                                    ),
+                                    child: TextButton(
+                                      child: Text('開始練習',
+                                        style: TextStyle(fontSize: 14, color: Colors.white,),),
+                                      onPressed: (){
+                                        AutoRouter.of(context).pushNamed("/${_routeName[_testIndex-1]}");
+                                      },
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Expanded(
+                                flex: 1,
+                                child: Container(),
+                              ),
+                            ],
+                          ),
                         ],
                       ),
                     )
-
                   ),
                   Expanded(
                     flex: 1,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Expanded(
-                          flex: 1,
-                          child: Visibility(
-                            visible: _testIndex != 0,
-                            child: Align(
-                              alignment: Alignment.bottomRight,
-                              child: TextButton(
-                                child: Text('Last'),
-                                onPressed: (){
-                                  setState(() {
-                                    _testIndex -= 1;
-                                    Navigator.pop(context);
-                                    introducePronunciationPractice();
-                                  });},
+                    child: Container(
+                      padding: EdgeInsets.only(top: 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Expanded(
+                            flex: 1,
+                            child: Visibility(
+                                visible: _testIndex != 0,
+                                child: CircleAvatar(
+                                  backgroundColor: PageTheme.app_theme_blue,
+                                  radius: 24.0,
+                                  child: IconButton(
+                                    icon: const Icon(Icons.navigate_before, color: Colors.white,),
+                                    onPressed: (){
+                                      setState(() {
+                                        _testIndex -= 1;
+                                        Navigator.pop(context);
+                                        introducePronunciationPractice();
+                                      });},
+                                  ),
+                                )
+                            ),
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: Text(
+                              '(${_testIndex+1}/${_introduction.length})',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: PageTheme.app_theme_blue,
+                                fontSize: 16,
                               ),
                             ),
                           ),
-                        ),
-                        Expanded(
-                          flex: 1,
-                          child: Visibility(
-                            visible: _testIndex < _introduction.length -1,
-                            child: Align(
-                              alignment: Alignment.bottomRight,
-                              child: TextButton(
-                                child: Text('Next'),
-                                onPressed: (){
-                                  setState(() {
-                                    _testIndex += 1;
-                                    Navigator.pop(context);
-                                    introducePronunciationPractice();
-                                  });},
-                              ),
+                          Expanded(
+                            flex: 1,
+                            child: Visibility(
+                                visible: _testIndex < _introduction.length -1,
+                                child: CircleAvatar(
+                                  backgroundColor: PageTheme.app_theme_blue,
+                                  radius: 24.0,
+                                  child: IconButton(
+                                    icon: const Icon(Icons.navigate_next, color: Colors.white,),
+                                    onPressed: (){
+                                      setState(() {
+                                        _testIndex += 1;
+                                        Navigator.pop(context);
+                                        introducePronunciationPractice();
+                                      });},
+                                  ),
+                                )
                             ),
                           ),
-                        ),
-                      ],
-                    ),
+                        ],
+                      ),
+                    )
                   ),
                 ],
               ),
